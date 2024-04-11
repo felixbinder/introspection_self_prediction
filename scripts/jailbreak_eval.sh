bf24ed80e1a5d55834c4cbe524e97acbff67005d
Rajashree Agrawal <rajashree.agrawal@gmail.com>
2024-04-11 22:57:43 +0000

    Setup for using 3.5-turbo as classifier

--- scripts/jailbreak_eval.sh
+++ scripts/jailbreak_eval.sh
@@ -3,7 +3,7 @@
 set -ex
 
 limit=1000
-note="2024-04-11-limit-$limit"
+note="2024-04-11-classifier-3.5-turbo-limit-$limit"
 
 python -m scripts.sweep_object_and_meta_levels \
         --study_name="jailbreaks" \
@@ -14,6 +14,6 @@ python -m scripts.sweep_object_and_meta_levels \
             cache_dir=/shared/exp/rajashree/jailbreaks/cache,
             language_model.logprobs=5,
             +response_property.language_model.logprops=5,
-            +response_property.language_model.model=gpt-4-turbo,
+            +response_property.language_model.model=gpt-3.5-turbo,
             +response_property.exclusion_rule_groups=[]" \
         --meta_overrides="prompt=meta_level/bare"
