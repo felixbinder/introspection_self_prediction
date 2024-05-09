baedcd032e51bc4d238ed4616cafa6c648ba1328
James Chua <chuajamessh@gmail.com>
2024-05-09 13:08:28 +0800

    fix naming

--- evals/response_property.py
+++ evals/response_property.py
@@ -227,7 +227,7 @@ def more_than_3_characters(row: pd.Series):
 def more_than_5_characters(row: pd.Series):
     return more_than_n_characters(row, 5)
 
-def knows_if_correct(row: pd.Series) -> str:
+def matches_target(row: pd.Series) -> str:
     # returns true or false as a string.
     # In `knows_if_correct.yaml`, we specify an exact match against 'true' or 'false'
     return row["target"].lower() == row["response"].strip().lower()
