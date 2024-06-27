45b4ac478e739c4c969d3fce4183bc5191b37ae1
Felix Binder <me@felixbinder.net>
2024-06-26 21:39:10 -0700

    making black/isort happy

--- scripts/sweep_full_study.py
+++ scripts/sweep_full_study.py
@@ -180,7 +180,9 @@ class StudyRunner:
             "--n_meta_val", type=int, help="Number of meta level completions for validation.", default=100
         )
         parser.add_argument("--skip_finetuning", action="store_true", help="Skip the finetuning step.", default=False)
-        parser.add_argument("--skip_finetuned_models", action="store_true", help="Do not run finetuned models.", default=False)
+        parser.add_argument(
+            "--skip_finetuned_models", action="store_true", help="Do not run finetuned models.", default=False
+        )
         parser.add_argument(
             "--skip_finetuning_for_models",
             type=str,
@@ -261,7 +263,7 @@ class StudyRunner:
 
     def get_finetuned_model_configs(self):
         """Pull out the config names of the finetuned models from the state file."""
-        if self.args.skip_finetuned_models: # we don't want to run finetuned models
+        if self.args.skip_finetuned_models:  # we don't want to run finetuned models
             return []
         return [v["ft_model_config"] for v in self.state["finetuning_runs"].values() if v["status"] == "complete"]
 
@@ -415,7 +417,7 @@ class StudyRunner:
                             f"n_train_items: {self.args.n_finetuning}",
                             train_folder,
                             val_folder,
-                            overwrite=False, # they get recreated only when missing
+                            overwrite=False,  # they get recreated only when missing
                         )
                         finetuning_folder_paths.append(yaml_path)
         print(f"Created {len(finetuning_folder_paths)} finetuning dataset configs. Creating datasets...")
