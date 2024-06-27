d758e0e7929acbf76ef5e2fb18da5e0d5d00f3fe
Felix <me@felixbinder.net>
2024-06-27 00:39:21 +0000

    updated hardcoded models in Anthropic API

--- evals/apis/inference/anthropic_api.py
+++ evals/apis/inference/anthropic_api.py
@@ -20,6 +20,7 @@ ANTHROPIC_MODELS = {
     "claude-3-haiku-20240307",
     "claude-3-sonnet-20240229",
     "claude-3-opus-20240229",
+    "claude-3-5-sonnet-20240620",
 }
 
 LOGGER = logging.getLogger(__name__)
