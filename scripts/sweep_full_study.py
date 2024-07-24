88a4a0d7ebea0b36992aafb56c1653c1e4464749
James Chua <chuajamessh@gmail.com>
2024-07-24 18:31:36 +0800

    undo sweep changes

--- scripts/sweep_full_study.py
+++ scripts/sweep_full_study.py
@@ -637,6 +637,8 @@ class StudyRunner:
                 for task, response_properties in combine_dicts_of_lists([self.args.tasks, self.args.val_tasks]).items():
                     for response_property in response_properties:
                         for prompt in self.args.prompt_configs:
+                            # pull the divergent strings
+                            divergent_strings_path = self.state["divergent_strings"][task]["strings_path"]
                             command = self.get_meta_level_command(
                                 model,
                                 task,
@@ -644,7 +646,7 @@ class StudyRunner:
                                 prompt,
                                 self.args.n_meta_val,
                                 "val",
-                                strings_path="none",
+                                divergent_strings_path,
                             )
                             if command not in self.state["meta_val_runs"]:
                                 self.state["meta_val_runs"].update(
