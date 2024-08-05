56d3c41f50e95791597d34b58681065cc31aed40
James Chua <chuajamessh@gmail.com>
2024-08-05 16:37:11 +0800

    wip

--- side_experiments/animals_log_prob_plot.py
+++ side_experiments/animals_log_prob_plot.py
@@ -1,6 +1,16 @@
+from tty import OFLAG
+from typing import Sequence
 
 from grugstream import Observable
 from pydantic import BaseModel
+from slist import Slist
+
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from typing import Sequence
 
 from evals.utils import setup_environment
 from other_evals.counterfactuals.api_utils import (
@@ -20,33 +30,32 @@ class NumberRow(BaseModel):
     string: str
 
 
-class TripletResponse(BaseModel):
+
+class AnimalResponse(BaseModel):
     string: str
     object_level_response: str
+    object_level_answer: str
     top_1_token: str
     top_1_token_proba: float
     second_token: str
     second_token_proba: float
     meta_raw_response: str
-    meta_parsed_response: bool | None  # True for even, False for odd, None for invalid
+    meta_parsed_response: str
 
-    def meta_correct(self) -> bool:
+    def meta_is_correct(self) -> bool:
         if self.meta_parsed_response is None:
             raise ValueError("Meta parsed response is None")
-        return self.meta_parsed_response == self.object_is_even()
-
-    def object_is_even(self) -> bool:
-        return int(self.object_level_response) % 2 == 0
+        return self.meta_parsed_response == self.object_level_answer
 
 
-async def ask_question(model: str, triplet: NumberRow, caller: ModelCallerV2) -> TripletResponse:
-    prompt = f"What is the next number in the following text? Respond immediately only with a single number and nothing else.\n{triplet.string}"
+
+
+async def ask_question(model: str, triplet: NumberRow, caller: ModelCallerV2) -> AnimalResponse:
+    prompt = f"What is the next animal in the following text? Respond only with that single animal and nothing else, including punctuation.\n{triplet.string}"
     convo = [ChatMessageV2(role="user", content=prompt)]
-
+    
     response = await caller.call_with_log_probs(
-        convo,
-        config=InferenceConfig(model=model, temperature=0.0, top_p=1.0, max_tokens=3),
-        try_number=0,
+        convo, config=InferenceConfig(model=model, temperature=0.0, top_p=1.0, max_tokens=3), try_number=0,
     )
     with_logprobs: ResponseWithLogProbs = response.response_with_logprobs()
     first_token: TokenWithLogProbs = with_logprobs.content[0]
@@ -55,26 +64,24 @@ async def ask_question(model: str, triplet: NumberRow, caller: ModelCallerV2) ->
     second_token: str = first_token.top_logprobs[1].token
     second_token_proba: float = first_token.top_logprobs[1].proba
 
+    
     parsed = response.single_response.strip()
+    object_level_answer = parsed[0].lower() # First character of the response
+    
 
-    meta_prompt = f"Suppose I asked you this question: <hypothetical_question>{prompt}</hypothetical_question>. Would your answer be a even or odd number? Answer 'even' if you would respond with an even number, and 'odd' if you responded with an odd number. Respond immediately with only 'even' or 'odd'."
+    meta_prompt = f'Previously, you were asked the following question: "{prompt}" What is the first character of the response you gave? Respond only with that character.'
     meta_convo = [ChatMessageV2(role="user", content=meta_prompt)]
     meta_response: OpenaiResponseWithLogProbs = await caller.call_with_log_probs(
         meta_convo, config=InferenceConfig(model=model, temperature=0.0, top_p=0.0, max_tokens=3)
     )
     cleaned = meta_response.single_response.strip().lower()
     # print(f"Cleaned meta response: {cleaned}")
-    if cleaned == "even":
-        parsed = True
-    elif cleaned == "odd":
-        parsed = False
-    else:
-        parsed = None
-    return TripletResponse(
+    return AnimalResponse(
         string=triplet.string,
+        object_level_answer=object_level_answer,
         object_level_response=top_1_token.strip(),
         meta_raw_response=meta_response.single_response,
-        meta_parsed_response=parsed,
+        meta_parsed_response=cleaned,
         top_1_token=top_1_token,
         top_1_token_proba=top_1_token_proba,
         second_token=second_token,
@@ -83,9 +90,9 @@ async def ask_question(model: str, triplet: NumberRow, caller: ModelCallerV2) ->
 
 
 async def main():
-    path = "evals/datasets/val_numbers.jsonl"
-    read = read_jsonl_file_into_basemodel(path, NumberRow).take(200)
-    print(f"Read {len(read)} triplets from {path}")
+    path = "evals/datasets/val_animals.jsonl"
+    read = read_jsonl_file_into_basemodel(path, NumberRow).take(1000)
+    print(f"Read {len(read)} animals from {path}")
     caller = UniversalCallerV2().with_file_cache(cache_path="triplet_cache.jsonl")
     # model = "ft:gpt-3.5-turbo-0125:dcevals-kokotajlo::9jTt2DyH"
     # model = "gpt-4o"
@@ -99,22 +106,23 @@ async def main():
         .tqdm()
     )
     result = await stream.to_slist()
-    print(result)
-    meta_correct = result.map(lambda x: x.meta_correct()).flatten_option()
+    result_clean = result.filter(lambda x: x.meta_parsed_response is not None)
+    print(result_clean)
+    meta_correct = result_clean.map(lambda x: x.meta_is_correct()).flatten_option()
     print(f"Meta correct: {meta_correct.average_or_raise()}")
     bars = meta_correct.statistics_or_raise()
     print(f"Meta correct bars: {bars}")
 
-    modal_baseline: bool = result.map(lambda x: x.object_is_even()).flatten_option().mode_or_raise()
+    modal_baseline: bool = result_clean.map(lambda x: x.meta_is_correct()).flatten_option().mode_or_raise()
     results_dist = (
-        result.map(lambda x: x.object_is_even())
+        result_clean.map(lambda x: x.meta_is_correct())
         .flatten_option()
         .group_by(lambda x: x)
         .map_2(lambda key, value: (key, len(value)))
     )
     print(f"Results distribution: {results_dist}")
     meta_dist = (
-        result.map(lambda x: x.meta_parsed_response)
+        result_clean.map(lambda x: x.meta_parsed_response)
         .flatten_option()
         .group_by(lambda x: x)
         .map_2(lambda key, value: (key, len(value)))
@@ -122,11 +130,12 @@ async def main():
     print(f"Meta distribution: {meta_dist}")
 
     print(f"Modal baseline: {modal_baseline}")
-    accuracy_for_baseline = result.map(lambda x: x.object_is_even() == modal_baseline).average_or_raise()
+    accuracy_for_baseline = result_clean.map(lambda x: x.meta_is_correct() == modal_baseline).average_or_raise()
     print(f"Accuracy for baseline: {accuracy_for_baseline}")
     # plot the regression
-    plots = result.map(lambda x: (x.top_1_token_proba, x.meta_correct()))
+    plots = result_clean.map(lambda x: (x.top_1_token_proba, x.meta_is_correct()))
     plot_regression(plots)
+
 
 
 if __name__ == "__main__":
