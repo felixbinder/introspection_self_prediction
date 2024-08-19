487041316d04bd163598ff838e201d08b7124d0d
James Chua <chuajamessh@gmail.com>
2024-08-19 16:21:52 -0700

    save refactor

--- evals/james_create_dataset.py
+++ evals/james_create_dataset.py
@@ -1,33 +1,45 @@
-import csv
 import json
 import logging
 from pathlib import Path
 from string import Template
+from typing import Sequence
 
 import pandas as pd
-import tqdm
+from attr import dataclass
+from omegaconf import OmegaConf
 from pydantic_core._pydantic_core import ValidationError
 
-from evals.analysis.loading_data import load_single_df
 from evals.data_models.messages import ChatMessage, MessageRole, Prompt, PromptTemplate
-from evals.load.lazy_object_level_llm_extraction import (
-    lazy_add_response_property_to_object_level,
-)
+from evals.load.lazy_object_level_llm_extraction import james_lazy_add_property
 
 LOGGER = logging.getLogger(__name__)
+
+
+def template_name_to_prompt_template(template_name: str) -> PromptTemplate:
+    # evals/conf/prompt/meta_level/template_name
+
+    yaml_path = Path(__file__).parent / "conf" / "prompt" / "meta_level" / f"{template_name}.yaml"
+    # omegaconf without resolving wth lol
+    conf = OmegaConf.load(yaml_path)
+    _conf = OmegaConf.to_container(conf, resolve=False)
+    return PromptTemplate.model_validate(_conf)
+
+
+@dataclass
+class GeneratedDataset:
+    train: Sequence[dict]
+    val: Sequence[dict]
 
 
 def generate_finetuning_dataset(
     train_base_dir: str,
     val_base_dir: str,
-    output_dir: Path,
-    name: str,
     response_property_name: str,
-    prompt_template: dict,  # todo: make it lookup the correct path
+    prompt_template: str,  # todo: make it lookup the correct path
     n_train_items: int,
     n_val_items: int,
     seed: int = 42,
-) -> tuple[Path, Path]:
+) -> GeneratedDataset:
     """
     Generate a dataset for finetuning.
 
@@ -37,7 +49,7 @@ def generate_finetuning_dataset(
         output_dir (Path): Path to save the generated dataset.
         name (str): Name of the dataset.
         response_property_name (str): Name of the response property in the data.
-        prompt_template (dict): Prompt template configuration.
+        prompt_template (str): Prompt template configuration.
         n_train_items (int, optional): Number of training items to use.
         n_val_items (int, optional): Number of validation items to use.
         train_strings_path (str, optional): Path to training strings file.
@@ -49,59 +61,54 @@ def generate_finetuning_dataset(
     Returns:
         tuple[Path, Path]: Paths to the generated training and validation datasets.
     """
-    train_filename = f"{name}_train_dataset.jsonl"
-    val_filename = f"{name}_val_dataset.jsonl"
-
-    train_filepath = output_dir / train_filename
-    val_filepath = output_dir / val_filename
-
     # Load and process training data
-    train_df = load_and_process_data(train_base_dir, response_property_name, n_train_items, seed)
-
+    train_df = load_and_process_data(
+        train_base_dir, response_property_name=response_property_name, seed=seed, n_items=n_train_items
+    )
     # Load and process validation data
-    val_df = load_and_process_data(val_base_dir, response_property_name, n_val_items, seed)
+    val_df = load_and_process_data(
+        val_base_dir, response_property_name=response_property_name, seed=seed, n_items=n_val_items
+    )
+    assert len(train_df) > 0, "No training data found."
+    assert len(val_df) > 0, "No validation data found."
 
-    # Generate messages and save to files
-    prompt_template_obj = PromptTemplate(**prompt_template)
-    generate_and_save_messages(train_df, prompt_template_obj, response_property_name, train_filepath)
-    generate_and_save_messages(val_df, prompt_template_obj, response_property_name, val_filepath)
-
-    # Save dataframes
-    train_df.to_csv(train_filepath.with_suffix(".df.csv"), index=False, quoting=csv.QUOTE_ALL)
-    val_df.to_csv(val_filepath.with_suffix(".df.csv"), index=False, quoting=csv.QUOTE_ALL)
-
-    return train_filepath, val_filepath
+    prompt_template_obj = template_name_to_prompt_template(prompt_template)
+    train = generate_and_save_messages(train_df, prompt_template_obj, response_property_name)
+    val = generate_and_save_messages(val_df, prompt_template_obj, response_property_name)
+    return GeneratedDataset(train=train, val=val)
 
 
-def load_and_process_data(base_dir, response_property_name, strings_path=None, n_items=None, seed=42):
-    df = load_single_df(Path(base_dir))
+def load_and_process_data(base_dir, response_property_name, seed, n_items=None):
+    # df = load_single_df(Path(base_dir))
+    path = Path(base_dir) / f"data{seed}.csv"
+    df = pd.read_csv(path)
+    assert len(df) > 0, f"No data found in {path}"
     # todo: just read in the df lol...
-    df = lazy_add_response_property_to_object_level(df, {}, response_property_name)
-
-    if strings_path:
-        strings = pd.read_csv(strings_path)["string"].values
-        df = df[df["string"].isin(strings)]
+    df = james_lazy_add_property(df, response_property_name)
+    assert response_property_name in df.columns, f"Response property {response_property_name} not in {path}"
 
     if n_items and n_items < len(df):
         df = df.sample(n_items, random_state=seed, replace=False)
 
     df = df.dropna(subset=[response_property_name])
     df = df[df[response_property_name] != "nan"]
+    assert len(df) > 0, f"No data found in {path} after filtering"
 
     return df
 
 
-def generate_and_save_messages(df, prompt_template, response_col, filepath):
-    with open(filepath, "w") as f:
-        for _, row in tqdm.tqdm(df.iterrows(), total=len(df)):
-            try:
-                prompt = process_prompt(row, prompt_template, response_col)
-                prompt = prompt.openai_finetuning_format()
-                prompt = json.loads(prompt)
-                prompt["string"] = row["string"]
-                f.write(json.dumps(prompt) + "\n")
-            except ValidationError as e:
-                LOGGER.warning(f"Failed row with error {e}")
+def generate_and_save_messages(df, prompt_template, response_col) -> Sequence[dict]:
+    output = []
+    for _, row in df.iterrows():
+        try:
+            prompt = process_prompt(row, prompt_template, response_col)
+            prompt = prompt.openai_finetuning_format()
+            prompt = json.loads(prompt)
+            prompt["string"] = row["string"]
+            output.append(prompt)
+        except ValidationError as e:
+            LOGGER.warning(f"Failed row with error {e}")
+    return output
 
 
 def process_prompt(row: pd.Series, prompt_template: PromptTemplate, response_col: str = "response") -> Prompt:
@@ -131,33 +138,21 @@ def process_prompt(row: pd.Series, prompt_template: PromptTemplate, response_col
     return Prompt(messages=messages)
 
 
-# ""
+###
+"""
+todo: fix
+{'messages': [{'role': 'system', 'content': ''}, {'role': 'user', 'content': 'Previously, you were asked the following question: "${task.prompt}" ${response_property.meta_level_prompt}\n'}, {'role': 'assistant', 'content': 'A'}], 'string': 'A jet mill  grinds materials by using a high speed jet of compressed air or'}
 
-# name: gpt-3.5-turbo_numbers_is_even_direct_minimal
-# defaults: # we need to use defaults here
-#   - task: numbers
-#   - prompt: meta_level/minimal
-#   - response_property: is_even_direct
-# train_base_dir: /Users/jameschua/ml/introspection_self_prediction_astra/exp/is_even_check/object_level_gpt-3.5-turbo-0125_object_level_minimal_prompt_numbers_train_task__note
-# val_base_dir: /Users/jameschua/ml/introspection_self_prediction_astra/exp/is_even_check/object_level_gpt-3.5-turbo-0125_object_level_minimal_prompt_numbers_val_task__note
-# enforce_unique_strings: False
-# n_train_items: 400
+may just be faster to rewrite everything?
+"""
 
-# """"""
-
-train_filepath, val_filepath = generate_finetuning_dataset(
-    train_base_dir="/Users/jameschua/ml/introspection_self_prediction_astra/exp/claude_data_try_3/object_level_claude-3-5-sonnet-20240620_object_level_minimal_prompt_wikipedia_long_train_task__note/data0.csv",
-    val_base_dir="/Users/jameschua/ml/introspection_self_prediction_astra/exp/claude_data_try_3/object_level_claude-3-5-sonnet-20240620_object_level_minimal_prompt_wikipedia_long_train_task__note/data0.csv",
-    output_dir=Path("test/"),
-    name="gpt-3.5-turbo_numbers_is_even_direct_minimal",
-    response_property_name="is_even_direct",
-    prompt_template={
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Please respond to: ${string}"},
-        ]
-    },
+out = generate_finetuning_dataset(
+    train_base_dir="/Users/jameschua/ml/introspection_self_prediction_astra/exp/claude_data_try_3/object_level_claude-3-5-sonnet-20240620_object_level_minimal_prompt_wikipedia_long_train_task__note",
+    val_base_dir="/Users/jameschua/ml/introspection_self_prediction_astra/exp/claude_data_try_3/object_level_claude-3-5-sonnet-20240620_object_level_minimal_prompt_wikipedia_long_train_task__note",
+    response_property_name="first_character",
+    prompt_template="minimal",
     n_train_items=1000,
     n_val_items=200,
-    seed=42,
-)
+    seed=0,
+).train[0]
+print(out)
